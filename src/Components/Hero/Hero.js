1c6c60646e9ac92f696c07c983aa279a8317a2a8
TU CAM KIET <camkiettu@gmail.com>
2025-08-16 20:20:40 +0700

    update frontend

---
+++ src/Components/Hero/Hero.js
@@ -0,0 +1,36 @@
+import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import hero_img from "../Assets/hero_img.png";
+
+const Hero = () => {
+  const hero_left_p = "text-[#171717] text-8xl font-extrabold";
+  return (
+    <div className="hero bg-[linear-gradient(180deg,#fde1ff,#e1ffea22_60%)] flex min-h-screen select-none">
+      <div className="hero-left flex flex-col flex-1 justify-center gap-5 pl-44 leading-none">
+        <h1 className="text-[#090909] text-2xl font-semibold">
+          NEW ARRIVALS ONLY
+        </h1>
+        <div>
+          <div className="hero-hand-icon flex items-center gap-5">
+            <p className={hero_left_p}>new</p>
+            <FontAwesomeIcon
+              icon="fa-regular fa-hand-peace"
+              className="text-8xl"
+            />
+          </div>
+          <p className={hero_left_p}>collections</p>
+          <p className={hero_left_p}>for everyone</p>
+        </div>
+        <div className="hero-latest-btn flex justify-center items-center gap-3.5 w-72 h-16 rounded-full mt-7 bg-[#ff4141] text-white text-2xl font-medium cursor-pointer">
+          <div>Last Collection</div>
+          <FontAwesomeIcon icon="fa-solid fa-arrow-right" />
+        </div>
+      </div>
+      <div className="hero-right flex flex-1 items-center justify-center">
+        <img src={hero_img} alt="hero" className="" />
+      </div>
+    </div>
+  );
+};
+
+export { Hero };
---
+++ src/Components/Navbar/Navbar.js
@@ -0,0 +1,97 @@
+import React, { useState } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { Link } from "react-router-dom";
+import shopping_bag from "../Assets/shopping_bag.png";
+
+const Navbar = () => {
+  const [menu, setMenu] = useState("shop");
+
+  //group className
+  const nav_menu_li =
+    "flex flex-col items-center justify-center sm:gap-y-[1vh] gap-y-[2px] cursor-pointer";
+  const nav_menu_hr = "border-none w-[80%] h-[2px] rounded-xl bg-[#FF4141]";
+
+  return (
+    <div className="navbar sticky top-0 bg-[#fde1ff] flex justify-between p-[max(2px,1vh)] shadow-[0_1px_3px_-2px_black] min-w-fit select-none">
+      <div className="nav-logo flex items-center gap-x-px cursor-pointer">
+        <img
+          src={shopping_bag}
+          className="h-[clamp(32px,10vmin,100px)] w-auto"
+          alt="logo"
+        />
+        <span className="text-[#171717] text-[clamp(16px,4.3vw,38px)] max-sm:hidden font-semibold">
+          SHOPPER
+        </span>
+      </div>
+      <ul className=" nav-menu flex items-center list-none gap-x-[2vw] text-[#626262] text-[clamp(12px,4vw,30px)] font-medium">
+        <li
+          className={nav_menu_li}
+          onClick={() => {
+            setMenu("shop");
+          }}
+        >
+          <Link style={{ textDecoration: "none" }} to="/">
+            Shop
+          </Link>
+          {menu === "shop" ? <hr className={nav_menu_hr} /> : ""}
+        </li>
+        <li
+          className={nav_menu_li}
+          onClick={() => {
+            setMenu("men");
+          }}
+        >
+          <Link style={{ textDecoration: "none" }} to="/men">
+            Men
+          </Link>
+          {menu === "men" ? <hr className={nav_menu_hr} /> : ""}
+        </li>
+        <li
+          className={nav_menu_li}
+          onClick={() => {
+            setMenu("women");
+          }}
+        >
+          <Link style={{ textDecoration: "none" }} to="/women">
+            Women
+          </Link>
+          {menu === "women" ? <hr className={nav_menu_hr} /> : ""}
+        </li>
+        <li
+          className={nav_menu_li}
+          onClick={() => {
+            setMenu("kids");
+          }}
+        >
+          <Link style={{ textDecoration: "none" }} to="/kids">
+            Kids
+          </Link>
+          {menu === "kids" ? <hr className={nav_menu_hr} /> : ""}
+        </li>
+      </ul>
+      <div className=" nav-login-cart flex items-center md:gap-x-[2vw] gap-x-[2px]">
+        <Link style={{ textDecoration: "none" }} to="/login">
+          <button
+            className="px-[2vw] sm:py-[2vh] py-[3px] outline-none border-[1px] border-[#7a7a7a] rounded-[1vmax] text-[#515151] text-[clamp(12px,4vw,30px)] font-medium cursor-pointer bg-white
+        active:bg-[#f3f3f3]"
+          >
+            Login
+          </button>
+        </Link>
+        <Link style={{ textDecoration: "none" }} to="/cart">
+          <div className="relative flex cursor-pointer">
+            <FontAwesomeIcon
+              icon="fa-solid fa-cart-shopping"
+              className="text-[clamp(12px,4vw,32px)]"
+            />
+            <span className="nav-cart-count absolute top-0 right-0 translate-x-1/4 -translate-y-1/2 bg-red-500 text-white font-medium rounded-full px-[10%] text-[clamp(9px,2vw,20px)]">
+              0
+            </span>
+          </div>
+        </Link>
+      </div>
+    </div>
+  );
+};
+
+export { Navbar };
