8f042047ea18a9bb46bcf928fbd7802c30231041
TU CAM KIET <camkiettu@gmail.com>
2025-08-28 13:21:40 +0700

    frontend-17

--- src/Components/Hero/Hero.js
+++ src/Components/Hero/Hero.js
@@ -4,15 +4,15 @@ import hero_img from "../Assets/hero_img.png";
 
 const Hero = () => {
   const hero_left_p =
-    "text-[#171717] text-[clamp(4rem,40vmin,6rem)] font-extrabold";
+    "text-[#171717] text-[clamp(2rem,20vmin,6rem)] font-extrabold";
   return (
     <div className="hero bg-[linear-gradient(180deg,#fde1ff,#e1ffea22_60%)] flex w-full">
-      <div className="hero-left flex flex-col flex-1 justify-center gap-[clamp(0.83rem,8.3vmin,1.25rem)] leading-none pl-[5vw]">
-        <h1 className="text-[#090909] text-[clamp(1rem,10vmin,1.5rem)] font-semibold">
+      <div className="hero-left flex flex-col flex-1 justify-center gap-[clamp(0.415rem,4.15vmin,1.25rem)] leading-none sm:pl-[5vw]">
+        <h1 className="text-[#090909] text-[clamp(0.5rem,5vmin,1.5rem)] font-semibold">
           NEW ARRIVALS ONLY
         </h1>
         <div>
-          <div className="hero-hand-icon flex items-center gap-[clamp(0.83rem,8.3vmin,1.25rem)] ">
+          <div className="hero-hand-icon flex items-center gap-[clamp(0.415rem,4.15vmin,1.25rem)] ">
             <p className={hero_left_p}>new</p>
             <FontAwesomeIcon
               icon="fa-regular fa-hand-peace"
@@ -22,7 +22,7 @@ const Hero = () => {
           <p className={hero_left_p}>collections</p>
           <p className={hero_left_p}>for everyone</p>
         </div>
-        <div className="hero-latest-btn flex justify-center items-center gap-[clamp(0.583rem,5.83vmin,0.875rem)] w-[clamp(12rem,120vmin,18rem)] h-[clamp(2.6rem,28.6vmin,4rem)] text-[clamp(1rem,10vmin,1.5rem)] mt-[clamp(0.116rem,11.6vmin,1.75rem)] rounded-full bg-[#ff4141] text-white font-medium cursor-pointer">
+        <div className="hero-latest-btn flex justify-center items-center gap-[clamp(0.291rem,2.91vmin,0.875rem)] w-[clamp(6rem,60vmin,18rem)] h-[clamp(1.4rem,14.3vmin,4rem)] text-[clamp(0.5rem,5vmin,1.5rem)] mt-[clamp(0.58rem,5.8vmin,1.75rem)] rounded-full bg-[#ff4141] text-white font-medium cursor-pointer">
           <div>Last Collection</div>
           <FontAwesomeIcon icon="fa-solid fa-arrow-right" />
         </div>
