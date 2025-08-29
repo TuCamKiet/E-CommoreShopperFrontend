520a3d2fbc3abcd92b18dc10ec96bf54e5026937
TU CAM KIET <camkiettu@gmail.com>
2025-08-29 13:38:12 +0700

    mobile responsive

--- src/Components/Offers/Offers.js
+++ src/Components/Offers/Offers.js
@@ -3,7 +3,7 @@ import exclusive_image from "../Assets/exclusive_image.png";
 
 const Offers = () => {
   const offers_left_h1 =
-    "text-[#171717] text-[clamp(1rem,10vmin,4.5rem)] font-semibold leading-none";
+    "text-[#171717] text-[clamp(1rem,9vmin,4.5rem)] font-semibold leading-none";
   return (
     <div className="offers w-full h-fit flex items-center justify-end relative border-2 border-black">
       <div className="offers-left flex items-center w-full h-[75%] min-h-fit absolute bg-[linear-gradient(180deg,#fde1ff,#e1ffea22_60%)] border-2 border-red-500">
@@ -13,7 +13,7 @@ const Offers = () => {
           <p className="text-[#171717] text-[clamp(0.4rem,4vmin,1.25rem)] font-semibold pt-[1.5vmin]">
             ONLY ON BEST SELLERS PRODUCTS
           </p>
-          <button className="w-[clamp(0.5rem,50vmin,16rem)] h-[clamp(1rem,10vmin,4rem)] rounded-full bg-[#ff4141] text-white text-[clamp(0.4rem,4vmin,1.25rem)] font-medium mt-[clamp(0.5rem,5vmin,1.5rem)] cursor-pointer">
+          <button className="w-[clamp(0.5rem,50vmin,16rem)] h-[clamp(1rem,10vmin,4rem)] rounded-full bg-[#ff4141] text-white text-[clamp(0.4rem,4vmin,1.25rem)] font-medium mt-[clamp(0.5rem,4vmin,1.5rem)] cursor-pointer">
             Check Now
           </button>
         </div>
