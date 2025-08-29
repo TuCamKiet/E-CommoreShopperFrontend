0f7cd72827458cc91d2dae3a4695242abfe459a9
TU CAM KIET <camkiettu@gmail.com>
2025-08-29 22:28:44 +0700

    mobile responsive

--- src/Components/NewsLetter/NewsLetter.js
+++ src/Components/NewsLetter/NewsLetter.js
@@ -2,14 +2,14 @@ import React from "react";
 
 const NewsLetter = () => {
   return (
-    <div className="newsletter h-fit flex flex-col items-center justify-center text-center pt-[clamp(0.415rem,4.15vmin,1.25rem)] px-[clamp(1.5rem,9vmin,6rem)] gap-[clamp(0.5rem,5vmin,1.75rem)] sm:leading-normal leading-none border-2 border-red-500 bg-[linear-gradient(180deg,#fde1ff,#e1ffea22_60%)]">
+    <div className="newsletter h-fit flex flex-col items-center justify-center text-center pt-[clamp(0.415rem,4.15vmin,1.25rem)] px-[clamp(1.5rem,9vmin,6rem)] gap-[clamp(0.5rem,5vmin,1.75rem)] sm:leading-normal leading-none bg-[linear-gradient(180deg,#fde1ff,#e1ffea22_60%)]">
       <h1 className="text-[#454545] text-[clamp(1rem,10vmin,3rem)] font-semibold">
         Get Exclusive Offers On Your Email
       </h1>
       <p className="text-[#454545] text-[clamp(0.5rem,5vmin,1.25rem)]">
         Subscribe to our newsletter and stay updated
       </p>
-      <div className="flex items-center justify-end bg-white w-[clamp(14rem,80vmin,45rem)] h-[clamp(1.4rem,14.3vmin,4rem)] rounded-full border-2 border-[#e3e3e3]">
+      <div className="flex items-center justify-end bg-white w-[clamp(14rem,140vmin,50rem)] h-[clamp(1.4rem,14.3vmin,4rem)] rounded-full border-[1px] border-[#e3e3e3]">
         <input
           type="email"
           placeholder="Your Email id"
