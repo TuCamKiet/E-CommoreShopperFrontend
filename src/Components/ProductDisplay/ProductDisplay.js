67a9d29b234d280a20caef6087c4762fb85f81bc
TU CAM KIET <camkiettu@gmail.com>
2025-12-01 20:15:49 +0700

    product display  interface

--- src/Components/ProductDisplay/ProductDisplay.js
+++ src/Components/ProductDisplay/ProductDisplay.js
@@ -4,39 +4,39 @@ import React, { useLayoutEffect, useState } from "react";
 const ProductDisplay = (props) => {
   const { product } = props;
 
-  const [rowToColReverse, setRowToColReverse] = useState(window.innerWidth <= 945);
-  useLayoutEffect(() => {
-    const handleResize = () => {
-      setRowToColReverse(window.innerWidth <= 945);
-    };
-    // 2. Attach the defined function to the resize event
-    window.addEventListener("resize", handleResize);
-    // 3. Clean up the listener when the component unmounts
-    return () => {
-      window.removeEventListener("resize", handleResize);
-    };
-  }, []);
+  // const [rowToColReverse, setRowToColReverse] = useState(window.innerWidth <= 945);
+  // useLayoutEffect(() => {
+  //   const handleResize = () => {
+  //     setRowToColReverse(window.innerWidth <= 945);
+  //   };
+  //   // 2. Attach the defined function to the resize event
+  //   window.addEventListener("resize", handleResize);
+  //   // 3. Clean up the listener when the component unmounts
+  //   return () => {
+  //     window.removeEventListener("resize", handleResize);
+  //   };
+  // }, []);
 
   const productdisplay_img_list_img = "h-[clamp(1.5rem,15vmin,5rem)] object-contain";
-  const productdisplay_right_sizes_div = "p-[clamp(0.2rem,2vmin,1rem)] bg-[#fbfbfb]  border-[#ebebeb] rounded-3xl border-2 cursor-pointer";
+  const productdisplay_right_sizes_div = "p-[clamp(0.2rem,2vmin,1rem)] bg-[#fbfbfb]  border-[#ebebeb] rounded-3xl border-2 hover:text-[#ff4141] hover:border-[#ff4141] hover:font-semibold cursor-pointer";
   return (
-    <div className="productdisplay flex w-10/12 max-md:flex-col gap-[clamp(0.2rem,2vmin,1rem)]  mx-auto my-[clamp(0.5rem,4vmax,2.5rem)] ">
-      <div className={`productdisplay-left flex ${rowToColReverse?"flex-col-reverse justify-end":""} gap-[clamp(0.2rem,2vmin,1rem)]  items-start`}>
-        <div className={`productdisplay-img-list flex ${rowToColReverse?"":"flex-col"} gap-[clamp(0.2rem,2vmin,1rem)]`}>
+    <div className="productdisplay flex max-md:flex-col gap-[clamp(0.5rem,5vmin,1.75rem)] px-[5%] my-[clamp(0.5rem,4vmax,2.5rem)] ">
+      <div className="productdisplay-left flex max-lg:flex-col-reverse max-lg:justify-end gap-[clamp(0.2rem,2vmin,1rem)] items-start">
+        <div className="productdisplay-img-list flex flex-shrink-0 lg:flex-col gap-[clamp(0.2rem,2vmin,1rem)]">
           <img src={product.image} alt={product.image} className={productdisplay_img_list_img} />
           <img src={product.image} alt={product.image} className={productdisplay_img_list_img} />
           <img src={product.image} alt={product.image} className={productdisplay_img_list_img} />
           <img src={product.image} alt={product.image} className={productdisplay_img_list_img} />
         </div>
-        <div className="productdisplay-img max-md:w-full">
+        <div className="productdisplay-img w-full">
           <img
-            className="productdisplay-main-img h-full md:max-h-[45rem] max-md:w-full object-contain"
+            className="productdisplay-main-img h-full md:max-h-[45rem] w-full object-contain"
             src={product.image}
             alt={product.image}
           />
         </div>
       </div>
-      <div className="productdisplay-right flex flex-col flex-1 ">
+      <div className="productdisplay-right flex flex-col flex-1">
         <h1 className="text-[#3d3d3d] text-[clamp(0.75rem,7.5vmin,2.25rem)] font-bold">{product.name}</h1>
         <div className="productdisplay-right-stars flex items-center mt-[clamp(0.25rem,2.5vmin,0.75rem)] gap-[clamp(0.1rem,1vmin,0.25rem)] text-[clamp(0.4rem,4vmin,1.25rem)] text-[#1c1c1c]">
           <FontAwesomeIcon icon="fa-solid fa-star" />
